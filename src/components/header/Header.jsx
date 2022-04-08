376fc0dd946c29b40058bf04b00157315de07d51
Tran Anh (e1900305) <namanh022@gmail.com>
2022-04-08 12:58:01 +0300

    change basket count

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,14 +1,20 @@
 import React from 'react'
 import './Header.css'
+import { Link } from 'react-router-dom'
 import SearchIcon from "@material-ui/icons/Search"
 import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket';
+import { useStateValue } from '../../StateProvider';
 function Header() {
+  const [{ basket }, dispatch] = useStateValue();
   return (
-      <div className='header'>
+    <div className='header'>
+        <Link to ='/'>
           <img
               className='header__logo'
               src='http://pngimg.com/uploads/amazon/amazon_PNG11.png'
           />
+        </Link>
+          
           <div className="header__search">
               <input type="text" className="header__searchInput" />
               <SearchIcon className="header__searchIcon" />
@@ -26,11 +32,14 @@ function Header() {
           <div className='header__option'>
             <span className="header__optionLineOne"> Your </span>
             <span className="header__optionLineTwo"> Prime </span>    
-          </div>
-          <div className="header__optionBasket">
+        </div>
+        
+        <Link to='/checkout'>
+           <div className="header__optionBasket">
             <ShoppingBasketIcon />
-            <span className="header__optionLineTwo header__basketCount">0</span>
+            <span className="header__optionLineTwo header__basketCount">{basket?.length}</span>
           </div>
+        </Link>
       </div>
     </div>
   )
