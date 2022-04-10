577fe28c87b6902a2aec192959928885c9cb3e7c
Tran Anh (e1900305) <namanh022@gmail.com>
2022-04-10 19:40:51 +0300

    add name in checkout page

--- src/components/checkout/Checkout.jsx
+++ src/components/checkout/Checkout.jsx
@@ -4,7 +4,7 @@ import CheckoutProduct from '../checkoutProduct/CheckoutProduct'
 import Subtotal from '../subtotal/Subtotal'
 import './Checkout.css'
 const Checkout = () => {
-  const [{ basket }, dispatch] = useStateValue();
+  const [{ basket,user }, dispatch] = useStateValue();
   return (
       <div className='checkout'>
         <div className="checkout__left">
@@ -12,7 +12,8 @@ const Checkout = () => {
                 className='checkout__ad'
                 src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg"
               alt="" />
-          <div>
+        <div>
+          <h3>Hello {user.email}</h3>
               <h2 className="checkout__title">Your shopping Basket</h2>
                 {basket.map(item =>
                   <CheckoutProduct
