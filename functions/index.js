0ddd77d000b5930c89bc3d5d7e058ded976d898f
Tran Anh (e1900305) <namanh022@gmail.com>
2022-04-14 04:04:02 +0300

    fix bug

--- functions/index.js
+++ functions/index.js
@@ -8,29 +8,31 @@ const stripe = require("stripe")('sk_test_51KnBPcL052rpaIp5HzXsPR7RsiqGYby0n5BvX
 //App config
 const app = express();
 
-//Middlewares
+// - Middlewares
 app.use(cors({ origin: true }));
 app.use(express.json());
 
-//API routes
-app.get('/', (request,response) => response.status(200).send('hello world'))
+// - API routes
+app.get("/", (request, response) => response.status(200).send("hello world"));
 
-app.post('/payments/create', async (request, respone) => {
-    const total = request.query.total;
+app.post("/payments/create", async (request, response) => {
+  const total = request.query.total;
 
-    console.log('Payment Request Recieved >>>', total)
-    
-    const paymentIntent = await stripe.paymentIntents.create({
-        amount: total, // subunits of the currency
-        currency: "usd",
-    });
-    
-    respone.status(201).send({
-        clientSecret: paymentIntent.client_secret
-    })
-})
-//listen command
-exports.api = functions.https.onRequest(app)
+  console.log("Payment Request Recieved BOOM!!! for this amount >>> ", total);
+
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: total, // subunits of the currency
+    currency: "usd",
+  });
+
+  // OK - Created
+  response.status(201).send({
+    clientSecret: paymentIntent.client_secret,
+  });
+});
+
+// - Listen command
+exports.api = functions.https.onRequest(app);
 
 //Example endpoint
 //http://localhost:5001/clone-91b56/us-central1/api
